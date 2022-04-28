de86099bd0dd21e3bf6abbfb9d4750787122880e
Sabasiva06 <sabasiva06@gmail.com>
2022-04-28 16:32:09 +0530

    1st push

---
+++ src/com/individuallogin/CreateNewAccount.java
@@ -0,0 +1,17 @@
+package com.individuallogin;
+
+public class CreateNewAccount {   
+
+	public static void main(String[] args) {     
+		
+		
+		String a = "java";
+		
+	    
+		
+		
+		
+		
+	}
+
+}
