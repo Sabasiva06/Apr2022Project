31f095f0d1721d1d9079e5a9b910c4f182d6c170
Sabasiva06 <sabasiva06@gmail.com>
2022-04-29 13:40:48 +0530

    B works

--- src/com/individuallogin/CreateNewAccount.java
+++ src/com/individuallogin/CreateNewAccount.java
@@ -1,17 +1,21 @@
 package com.individuallogin;
 
-public class CreateNewAccount {   
+public class CreateNewAccount {
 
-	public static void main(String[] args) {     
-		
-		
+	public static void main(String[] args) {
+
 		String a = "java";
-		
-	    
-		
-		
-		
-		
+		String s = "Selenium";
+		String out = "";
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			char c = s.charAt(i);
+			out = out + c;
+
+		}
+
+		System.out.println("Revese of S :" + out);
+
 	}
 
 }
